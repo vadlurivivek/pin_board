729570080f8e26cf6a1611dc0bb8c1f19d9036ea
vivek <vadlurivivek@gmail.com>
2016-08-13 16:27:07 +0530

    update and edit added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,17 +1,42 @@
 class PinsController < ApplicationController
 	def index
+		@pins = Pin.all.order("created_at DESC")
 	end
 
 	def new
 		@pin = Pin.new
 	end
+
 	def create
 		@pin = Pin.new(pin_params)
+		if @pin.save
+			redirect_to @pin, notice: "Successfully created new Pin"
+		
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@pin = Pin.find(params[:id])
+	end
+
+	def edit ##this will need a template where as update doesnt just think update works on background like php file which updates
+		@pin = Pin.find(params[:id])
+	end
+
+	def update
+		@pin = Pin.find(params[:id])
+		if @pin.update_attributes(pin_params)
+			redirect_to @pin, notice:"pin was Successfully updated"
+		else
+			render 'edit'## render always looks for methods not pages it can also look for pages
+		end
 	end
 
 	private
+		def pin_params
+			params.require(:pin).permit(:title,:description)
+		end
 
-	def pin_params
-		params.require(:pin).permit(:title,:description)
-	end
 end
