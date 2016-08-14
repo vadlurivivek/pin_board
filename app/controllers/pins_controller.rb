c218b948babe25a3913c907f566958d77be43806
vivek <vadlurivivek@gmail.com>
2016-08-15 04:35:27 +0530

    final,tweaked somestyling in index and show, styled form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,6 @@
 class PinsController < ApplicationController
+	before_action :authenticate_user!, except:[:index,:show]
+
 	def index
 		@pins = Pin.all.order("created_at DESC")
 	end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ class Pin < ApplicationRecord
 	belongs_to :user
 	has_attached_file :image , styles: {medium: "300X300>"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :image, presence: true
 end
