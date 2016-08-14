063b9a09775521820b46fcd098569863db667fdc
vivek <vadlurivivek@gmail.com>
2016-08-14 14:53:09 +0530

    solved bugs about devise and added image uploading

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,11 +4,11 @@ class PinsController < ApplicationController
 	end
 
 	def new
-		@pin = Pin.new
+		@pin = current_user.pins.build
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
+		@pin = current_user.pins.build(pin_params)
 		if @pin.save
 			redirect_to @pin, notice: "Successfully created new Pin"
 		
@@ -41,7 +41,7 @@ class PinsController < ApplicationController
 
 	private
 		def pin_params
-			params.require(:pin).permit(:title,:description)
+			params.require(:pin).permit(:title,:description,:image)
 		end
 
 end
